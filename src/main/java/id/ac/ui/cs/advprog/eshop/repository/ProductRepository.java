c144426e4df2566248f2ada87a1a0c8b953666fd
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-02-06 15:31:15 +0800

    add ProductRepository.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public class ProductRepository {
+    private static List<Product> productData = new ArrayList<>();
+
+    public Product create(Product product) {
+        productData.add(product);
+        return product;
+    }
+
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
+}
